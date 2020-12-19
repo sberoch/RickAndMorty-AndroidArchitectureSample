ba8a7e4cac18770ef3eb3b733828305288fa3414
gokberkyagci <gyagci@invio.com>
2020-12-19 17:58:07 +0300

    Added SavedStateHandle to avoid unnecessary repository call

--- app/src/main/java/com/example/rickandmorty/ui/characterdetail/CharacterDetailViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/ui/characterdetail/CharacterDetailViewModel.kt
@@ -1,28 +1,21 @@
 package com.example.rickandmorty.ui.characterdetail
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
+import androidx.lifecycle.*
 import com.example.rickandmorty.data.entities.Character
 import com.example.rickandmorty.data.repository.CharacterRepository
 import com.example.rickandmorty.utils.Resource
+import java.lang.Exception
 
 class CharacterDetailViewModel @ViewModelInject constructor(
-    private val repository: CharacterRepository
+    @Assisted private val savedStateHandle: SavedStateHandle,
+    repository: CharacterRepository
 ) : ViewModel() {
 
-    private val _id = MutableLiveData<Int>()
-
-    private val _character = _id.switchMap { id ->
-        repository.getCharacter(id)
-    }
-    val character: LiveData<Resource<Character>> = _character
-
-
-    fun start(id: Int) {
-        _id.value = id
-    }
-
+    val character: LiveData<Resource<Character>> =
+        repository.getCharacter(
+            savedStateHandle.get<Int>("id")
+                ?: throw Exception("You need to pass character id")
+        )
 }
